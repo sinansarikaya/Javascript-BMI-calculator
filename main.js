08bafde5a501f3612b5a932b771d6bf7fb4252ca
Sinan <sinan.sarykaya@gmail.com>
2022-08-09 16:48:50 +0200

    template changed

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const height = document.getElementById("height");
 const weight = document.getElementById("weight");
-const info = document.querySelector(".modal-body");
-
+const info = document.querySelector(".info");
+const footer = document.getElementById("footer");
 
 
 const handleClick = () => {
+
+  console.log(height.value, weight.value)
 
   function numberWithCommas(num) {
     return num.toString().replace(/\B(?=(\d{2})+(?!\d))/g, '.');
@@ -12,10 +14,9 @@ const handleClick = () => {
   let newHeight = numberWithCommas(Number(height.value));
   console.log(newHeight)
 
-
-
   const index = weight.value / (newHeight * newHeight);
-  info.innerHTML += index;
+  info.innerHTML = index;
 
+  footer.style.height = "30vh";
 
 };
